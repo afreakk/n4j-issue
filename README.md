4ac00d86a39d8b822bd25e541045aeafd2646ae8
afreakk <laderud@hotmail.com>
2023-10-30 08:24:17 +0100

    yup

---
+++ README.md
@@ -0,0 +1,8 @@
+# Example of neo4j/graphql issue
+
+Run tests by executing following commands:
+
+```
+yarn install
+./test.sh watch-test
+```
---
+++ src/index.test.js
@@ -0,0 +1,117 @@
+import { readFileSync } from 'fs';
+import { ApolloServer } from '@apollo/server';
+import { join } from 'path';
+import { Neo4jGraphQL } from '@neo4j/graphql';
+import { it, describe, expect, test } from 'vitest';
+import { gql } from '@apollo/client/core';
+import neo4j from 'neo4j-driver';
+const driver = neo4j.driver(
+    process.env.NEO4J_DATABASE_HOST,
+    neo4j.auth.basic(
+        process.env.NEO4J_DATABASE_USERNAME,
+        process.env.NEO4J_DATABASE_PASSWORD
+    )
+);
+
+describe('testing graphql', async () => {
+    await driver.session().run(` match (n) detach delete n`);
+    const myUserId = Math.random().toString(36).slice(2, 7);
+    const neo4jGraphql = new Neo4jGraphQL({
+        typeDefs: readFileSync(
+            join(__dirname, 'schema.graphql'),
+            'utf8'
+        ).toString(),
+        driver,
+        features: {
+            populatedBy: {
+                callbacks: {
+                    getUserIDFromContext: () => 'hi',
+                },
+            },
+        },
+    });
+    const apolloServer = new ApolloServer({
+        schema: await neo4jGraphql.getSchema(),
+        introspection: true,
+    });
+    it('create tenant', async () => {
+        const r = await apolloServer.executeOperation(
+            {
+                query: gql`
+                    mutation addTenant($input: [TenantCreateInput!]!) {
+                        createTenants(input: $input) {
+                            tenants {
+                                id
+                                admins {
+                                    userId
+                                }
+                                settings {
+                                    openingDays {
+                                        open {
+                                            name
+                                        }
+                                    }
+                                    myWorkspace {
+                                        workspace
+                                    }
+                                }
+                            }
+                        }
+                    }
+                `,
+                variables: {
+                    input: {
+                        admins: {
+                            create: {
+                                node: { userId: myUserId },
+                            },
+                        },
+                        settings: {
+                            create: {
+                                node: {
+                                    myWorkspace: {
+                                        create: {
+                                            node: {
+                                                workspace: 'myWorkspace',
+                                            },
+                                        },
+                                    },
+                                    openingDays: {
+                                        create: {
+                                            node: {
+                                                open: {
+                                                    create: {
+                                                        node: {
+                                                            name: 'lambo',
+                                                        },
+                                                    },
+                                                },
+                                            },
+                                        },
+                                    },
+                                },
+                            },
+                        },
+                    },
+                },
+            },
+            { contextValue: { jwt: { id: myUserId } } }
+        );
+        expect(r).toMatchObject({
+            body: {
+                singleResult: {
+                    data: {
+                        createTenants: {
+                            tenants: [
+                                {
+                                    id: expect.any(String),
+                                    admins: [{ userId: myUserId }],
+                                },
+                            ],
+                        },
+                    },
+                },
+            },
+        });
+    });
+});
